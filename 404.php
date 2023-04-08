e0289894ca0866a6343321f49c006a8aa39ace1f
krslnkvl <119626008+krslnkvl@users.noreply.github.com>
2023-04-08 14:38:12 +0500

    Add files via upload

---
+++ 404.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/404.css">
+    <title>404</title>
+</head>
+<body>
+    <div class="container">
+        <ul>
+            <li>4</li>
+            <li><img src="img/flower.png" alt=""></li>
+            <li>4</li>
+        </ul>
+        <div class=error>
+            <p>Ошибка!</p>
+            <p>К сожалению, запрашиваемая Вами страница не найдена...</p>
+        </div>
+    </div>
+</body>
+</html>
